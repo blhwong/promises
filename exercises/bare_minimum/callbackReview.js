111619f6fa2a8ca0971e582574a7da6672a3037f
Brandon Wong <b.lh.wong@gmail.com>
2017-03-02 23:25:09 -0800

    Pass tests for pluckFirstLineFrommFile

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -8,6 +8,21 @@ var request = require('request');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFile = function (filePath) {
   // TODO
+  // console.log(filePath);
+  // var err = {};
+  // err['code'] = 'ENOENT';
+  var cb = arguments[1];
+  fs.readFile(filePath, function(err, content) {
+    var result;
+    if (err) {
+      console.log('fs.readFile failed :(\n', err);
+    } else {
+      console.log('fs.readFile successfully completed :)\n', content.toString());
+      var content = content.toString();
+      var result = content.split('\n')[0];
+    }
+    cb(err, result);
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
