b7a6f2461fbed12875a1f4a90f3fb9e7fbcaf293
Brandon Wong <b.lh.wong@gmail.com>
2017-03-03 00:04:23 -0800

    Create promise instance for pluckFirstLineFromFileAsync

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,6 +11,28 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  // var cb = arguments[1];
+  // console.log('callback', cb);
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, function(err, content) {
+      // reject(err);
+      // var result = content.toString().split('\n')[0];
+      // resolve(result);
+      var result;
+      if (err) {
+        // reject(new Error('fs.readFile failed:(\n', err));
+        reject(err);
+      } else {
+        console.log('fs.readFile successfully completed :)\n', content.toString());
+        var content = content.toString();
+        result = content.split('\n')[0];
+        console.log('this is the result', result);
+      }
+      resolve(result);
+      // cb(err, result);
+    });
+  });
+  // return promise;
 };
 
 // This function should retrieve the status code of a GET request to `url`
